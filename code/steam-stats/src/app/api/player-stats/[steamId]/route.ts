19db32f03a5c822faf1c5c701bf03734a6884b80
Rakesh Ayyaswami <rakeshta@gmail.com>
2024-01-28 03:55:03 +1100

    refactored server route into multiple helper functions + paralleized game details fetching

--- code/steam-stats/src/app/api/player-stats/[steamId]/route.ts
+++ code/steam-stats/src/app/api/player-stats/[steamId]/route.ts
@@ -3,45 +3,75 @@ import { sortBy } from 'lodash';
 import { ApiError } from '@models/errors';
 import { apiRouteWrapper } from '@models/helpers';
 import { SteamApi } from '@models/steam';
-import { ApiErrorCode, PlayerStats } from '@models/types';
+import { ApiErrorCode, PlayerGameStats, PlayerStats } from '@models/types';
 
 const MOST_PLAYED_GAME_COUNT = 10;
 
-export const GET = apiRouteWrapper<{ steamId: string }>(async (req, { params }) => {
+// local types ---------------------------------------------------------------------------------------------------------
+
+type PlayerSummary = Omit<PlayerStats, 'mostPlayedGames'>;
+
+// helpers -------------------------------------------------------------------------------------------------------------
+
+async function _fetchPlayerSummary(steamId: string): Promise<PlayerSummary> {
   // fetch data from Steam API. abort if no data found
-  const plyerSummariesRes = await SteamApi.fetchPlayerSummaries([params.steamId]);
-  if (plyerSummariesRes.length === 0) {
+  const plyerSummaries = await SteamApi.fetchPlayerSummaries([steamId]);
+  if (plyerSummaries.length === 0) {
     throw new ApiError(404, ApiErrorCode.InvalidSteamId, 'Invalid Steam ID');
   }
 
-  // fetch list of owned games for the player
-  const ownedGamesRes = await SteamApi.fetchOwnedGames(params.steamId);
-
-  // get detailed info for all owned games
-  const gamesDetails = [];
-  for (const game of ownedGamesRes) {
-    try {
-      const appDetails = await SteamApi.fetchAppDetails(game.appid);
-      gamesDetails.push({
-        appId: game.appid,
-        name: appDetails.name,
-        imageUrl: appDetails.header_image,
-        playtime: game.playtime_forever,
-      });
-    } catch (e) {
-      // log & skip this game
-      console.error(`Failed to fetch details for game ${game.appid}:`, e);
-    }
-  }
-
-  // compose response
-  const playerSummary = plyerSummariesRes[0];
+  // compose summary object
+  const playerSummary = plyerSummaries[0];
   return {
     steamId: playerSummary.steamid,
     displayName: playerSummary.personaname,
     avatarUrl: playerSummary.avatarfull,
     createdAt: playerSummary.timecreated,
     lastLogoffAt: playerSummary.lastlogoff,
-    mostPlayedGames: sortBy(gamesDetails, 'playtime').reverse().slice(0, MOST_PLAYED_GAME_COUNT),
+  } satisfies PlayerSummary;
+}
+
+async function _fetchMostPlayedGames(steamId: string): Promise<PlayerGameStats[]> {
+  // fetch list of owned games for the player & take the top N most played games
+  const ownedGames = await SteamApi.fetchOwnedGames(steamId);
+  const mostPlayedGames = sortBy(ownedGames, 'playtime_forever').reverse().slice(0, MOST_PLAYED_GAME_COUNT);
+
+  // fetch game details for all most played games in parallel
+  const mostPlayedGameDetails: PlayerGameStats[] = [];
+  const fetchPromises = mostPlayedGames.map(async (game) => {
+    try {
+      // fetch & parse game details
+      const appDetails = await SteamApi.fetchAppDetails(game.appid);
+      mostPlayedGameDetails.push({
+        appId: game.appid,
+        name: appDetails.name,
+        imageUrl: appDetails.header_image,
+        playtime: game.playtime_forever,
+      });
+    } catch (e) {
+      // some games may no longer be on the store.
+      // in that case, log error & skip it
+      console.error(`Failed to fetch details for game ${game.appid}:`, e);
+    }
+  });
+
+  // wait for all fetches to complete
+  await Promise.all(fetchPromises);
+
+  // return the list of most played games
+  return mostPlayedGameDetails;
+}
+
+// route handler -------------------------------------------------------------------------------------------------------
+
+export const GET = apiRouteWrapper<{ steamId: string }>(async (req, { params }) => {
+  // fetch player summary & most played games
+  const playerSummary = await _fetchPlayerSummary(params.steamId);
+  const mostPlayedGames = await _fetchMostPlayedGames(params.steamId);
+
+  // compose response
+  return {
+    ...playerSummary,
+    mostPlayedGames,
   } satisfies PlayerStats;
 });
