129252a8aae53ce5828db6ba95f8dc75d9441404
Rakesh Ayyaswami <rakeshta@gmail.com>
2024-01-29 04:45:31 +1100

    handle no games to show in the most played list

--- code/steam-stats/src/app/player-stats/[steamId]/MostPlayed.tsx
+++ code/steam-stats/src/app/player-stats/[steamId]/MostPlayed.tsx
@@ -11,11 +11,17 @@ export function MostPlayed({ className, stats }: Props) {
   return (
     <div className={className}>
       <h2 className='text-2xl font-bold mb-4'>Most Played Games</h2>
-      <div className='flex flex-col space-y-4'>
-        {stats.mostPlayedGames.map((game) => (
-          <GameCard key={game.appId} game={game} />
-        ))}
-      </div>
+      {stats.mostPlayedGames.length ?
+        <div className='flex flex-col space-y-4'>
+          {stats.mostPlayedGames.map((game) => (
+            <GameCard key={game.appId} game={game} />
+          ))}
+        </div>
+      : <div className='text-lg text-center py-20 '>
+          <p>Wow! Such Empty!</p>
+          <p className='text-muted'>Come back when you have played some games to see your most played list.</p>
+        </div>
+      }
     </div>
   );
 }
