f1285599b700f2391423d58f1f4bda74fd4b5144
Rakesh Ayyaswami <rakeshta@gmail.com>
2024-01-29 03:16:27 +1100

    made player summary responsive

--- code/steam-stats/src/app/player-stats/[steamId]/PlayerSummary.tsx
+++ code/steam-stats/src/app/player-stats/[steamId]/PlayerSummary.tsx
@@ -12,25 +12,23 @@ export interface Props {
 
 export function PlayerSummary({ className, stats }: Props) {
   return (
-    <Card className={clsx('p-4 flex flex-row items-stretch', className)}>
-      <img className='rounded-md rotate-3' src={stats.avatarUrl} alt='Player Avatar' />
+    <Card className={clsx('p-4 flex flex-col items-center md:flex-row md:items-stretch', className)}>
+      <img className='rounded-md rotate-3 w-60 self-center' src={stats.avatarUrl} alt='Player Avatar' />
       <div className='ml-4 flex flex-col justify-between'>
         <CardHeader>
           <CardTitle>{stats.displayName}</CardTitle>
           <CardDescription>On Steam since {DateFormat.long(stats.createdAt * 1000)}</CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className='flex flex-row space-x-8'>
-            <div className='flex flex-col'>
-              <span className='text-lg font-semibold'>{stats.ownedGameCount}</span>
-              <span className='text-sm text-muted-foreground'>Games owned</span>
-            </div>
-            <div className='flex flex-col'>
-              <span className='text-lg font-semibold'>
-                {(stats.totalPlaytime / 60).toLocaleString(undefined, { maximumFractionDigits: 0 })}
-              </span>
-              <span className='text-sm text-muted-foreground'>Lifetime hours played</span>
-            </div>
+        <CardContent className='flex flex-row space-x-8'>
+          <div className='flex flex-col'>
+            <span className='text-lg font-semibold'>{stats.ownedGameCount}</span>
+            <span className='text-sm text-muted-foreground'>Games owned</span>
+          </div>
+          <div className='flex flex-col'>
+            <span className='text-lg font-semibold'>
+              {(stats.totalPlaytime / 60).toLocaleString(undefined, { maximumFractionDigits: 0 })}
+            </span>
+            <span className='text-sm text-muted-foreground'>Lifetime hours played</span>
           </div>
         </CardContent>
       </div>
