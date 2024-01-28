cb0f7b5a26b4ba569686de2c15579e6a959c0d54
Rakesh Ayyaswami <rakeshta@gmail.com>
2024-01-29 03:12:30 +1100

    simple footer

--- code/steam-stats/src/app/layout.tsx
+++ code/steam-stats/src/app/layout.tsx
@@ -20,8 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={clsx(inter.className, 'dark')}>
+      <body className={clsx(inter.className, 'dark min-h-dvh flex flex-col justify-between')}>
         <div className='container mx-auto px-12 py-8'>{children}</div>
+        <footer className='container flex justify-center py-2'>
+          <p className='text-sm text-muted'>A project by Rakesh Ayyaswami</p>
+        </footer>
       </body>
     </html>
   );
