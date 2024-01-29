2bbd11dd981715ea31c36899c421caea44666fbf
Rakesh Ayyaswami <rakeshta@gmail.com>
2024-01-30 00:59:27 +1100

    temporary workaround for error handling

--- code/steam-stats/src/app/player-stats/[steamId]/error.tsx
+++ code/steam-stats/src/app/player-stats/[steamId]/error.tsx
@@ -8,13 +8,13 @@ export interface Props {
   error: Error;
 }
 
-export default function PlayerStatsError({ error }: Props) {
+export default function PlayerStatsError() {
   return (
-    <main className='flex-1 flex flex-col items-center justify-center space-y-8'>
-      <h1 className='text-2xl font-bold'>{error.message}</h1>
-      <p className='text-lg text-center'>Whoops! Something didn&#39;t go according to plan</p>
+    <main className='flex-1 flex flex-col items-center justify-center'>
+      <h1 className='text-2xl font-bold text-center'>Whoops! Something went wrong.</h1>
+      <p className='text-lg text-slate-300 text-center mt-2'>Check your Steam ID and try again.</p>
 
-      <Button className='mt-4' asChild>
+      <Button className='mt-8' asChild>
         <Link href='/'>Try Again</Link>
       </Button>
     </main>
